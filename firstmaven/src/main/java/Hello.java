daa72c46ff90a073c79f5620eba483effc2d0b15
hongtao <894021573@qq.com>
2019-03-04 10:16:14 +0800

    add

--- firstmaven/src/main/java/Hello.java
+++ firstmaven/src/main/java/Hello.java
@@ -1,7 +1,10 @@
+import java.util.UUID;
+
 public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("first");
+       // System.out.println("first");
+        System.out.println(UUID.randomUUID());
     }
 }
