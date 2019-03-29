371a7340a63be48b9c575137bb974bf73e0ed4aa
hongtao <894021573@qq.com>
2019-03-29 17:58:07 +0800

    add

---
+++ base/src/数组/Test.java
@@ -0,0 +1,12 @@
+package 数组;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        // 最后一个元素可以加逗号
+        int[] nums = {1, 2, 3,};
+        int[] nums2 = new int[4];
+        int[] nums3 = new int[]{1, 2, 3,};
+    }
+}
